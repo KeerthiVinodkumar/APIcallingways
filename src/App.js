e4c79c12b1f09b35059f0b5ac4b45cd6f8dffdb4
KeerthiVinodkumar <vinodkumarkeerthi725@gmail.com>
2023-05-29 16:12:04 +0530

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useEffect,useState } from 'react';
 function App() {
+  const [data,setdata]=useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts').then(
+        response=>response.json()
+    ).then(json=>setdata(json))
+})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {data.map(item=><li key={item.id}>{item.title}</li>)} */}
+      {data.map(item=><div class="card" style={{width: "18rem"}}>
+  
+  <div class="card-body">
+    <h5 class="card-title" key={item.id}>{item.title}</h5>
+    <p class="card-text">{item.body}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>)}
+      
     </div>
   );
 }
